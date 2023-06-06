1e15a19b127795931630e1f0d62625d837a64aa6
Brandon Yoon <122177926+brand0n-y00n@users.noreply.github.com>
2023-06-06 16:24:46 -0700

    started add application form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,55 @@
-import * as React from 'react';
+import React from 'react';
+import { useState } from 'react';
 // import { NextUIProvider } from '@nextui-org/react';
 import MainComponent from './components/mainContainer/MainComponent.jsx';
-
+import AddApplication from './components/mainContainer/AddApplication.jsx';
 export default function App() {
+  const columns = [
+    {
+      key: 'company_name',
+      label: 'Company Name',
+    },
+    {
+      key: 'date_applied',
+      label: 'Date Applied',
+    },
+    {
+      key: 'position_name',
+      label: 'Position',
+    },
+    {
+      key: 'city_name',
+      label: 'City',
+    },
+    {
+      key: 'notes_txt',
+      label: 'Notes',
+    },
+    {
+      key: 'status',
+      label: 'Status',
+    },
+    {
+      key: 'application_type',
+      label: 'Application Type',
+    },
+    {
+      key: 'listing_link',
+      label: 'Link To Listing',
+    },
+    {
+      key: 'action',
+      label: '',
+    },
+  ];
+
+  const [rows, setRows] = useState([])
+  
   return (
     <div>
       <h1>This is App</h1>
-      <MainComponent />
+      <AddApplication/>
+      <MainComponent columns={columns} rows={rows} />
     </div>
   );
 }
--- src/components/mainContainer/AddApplication.jsx
+++ src/components/mainContainer/AddApplication.jsx
@@ -1,1 +1,124 @@
-import React from 'react';
+import React , {useState} from "react";
+import { Dropdown, Modal, Button, Text, Input, Row, Checkbox } from "@nextui-org/react";
+// import { Mail } from "./Mail";
+// import { Password } from "./Password";
+
+export default function AddApplication() {
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState('Status');
+  
+  const handler = () => setVisible(true);
+
+  const closeHandler = () => {
+    setVisible(false);
+    console.log("closed");
+  };
+
+  return (
+    <div>
+      <Button auto shadow onPress={handler}>
+        Add Job Application
+      </Button>
+      <Modal
+        closeButton
+        aria-labelledby="modal-title"
+        open={visible}
+        onClose={closeHandler}
+      >
+        <Modal.Header>
+          <Text id="modal-title" size={18}>
+            Add New Job Application
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Company Name"
+            // contentLeft={<Mail fill="currentColor" />}
+          />
+          
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Date Applied"
+            // contentLeft={<Password fill="currentColor" />}
+          />
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Position"
+            // contentLeft={<Mail fill="currentColor" />}
+          />
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="City"
+            // contentLeft={<Mail fill="currentColor" />}
+          />
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Notes"
+            // contentLeft={<Mail fill="currentColor" />}
+          />
+          <Dropdown>
+            <Dropdown.Button flat>Status</Dropdown.Button>
+            <Dropdown.Menu aria-label="Single selection actions">
+              <Dropdown.Item key="applied">Applied</Dropdown.Item>
+              <Dropdown.Item key="phoneScreen">Phone Screen</Dropdown.Item>
+              <Dropdown.Item key="behavioralInterview">Behavioral Interview</Dropdown.Item>
+              <Dropdown.Item key="technicalInterview">
+                Technical Interview
+              </Dropdown.Item>
+              <Dropdown.Item key="ghosted">Ghosted</Dropdown.Item>
+              <Dropdown.Item key="offer">Offer</Dropdown.Item>
+              <Dropdown.Item key="rejectedOffer">Rejected Offer</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Application Type"
+            // contentLeft={<Mail fill="currentColor" />}
+          />
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Link To Listing"
+            // contentLeft={<Mail fill="currentColor" />}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button auto flat color="error" onPress={closeHandler}>
+            Close
+          </Button>
+          <Button auto onPress={closeHandler}>
+            Done
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
